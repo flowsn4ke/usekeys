d0ab20acd5057df9eab5daf4c40c84b8b1dc359f
Theo Louvel <louveltheophile@gmail.com>
2022-05-25 12:07:48 +0200

    change random id generator

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { useRef } from "react"
-import { nanoid } from "nanoid"
+import Snowflake from "../libs/snowflake"
 
 export default function useKeys() {
   const keys = useRef([])
+  const snowflake = useRef(new Snowflake)
 
   function makeKey(i) {
     while (i >= keys.current.length)
-      keys.current.push(nanoid())
+      keys.current.push(snowflake.current.generate())
 
     return keys.current[i]
   }
